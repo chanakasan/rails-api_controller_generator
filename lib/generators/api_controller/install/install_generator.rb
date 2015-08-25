c9246a12e85180ec57f46acb55a27de4c56168ac
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-08-25 07:31:44 +0530

    generate inflection rule

--- lib/generators/api_controller/install/install_generator.rb
+++ lib/generators/api_controller/install/install_generator.rb
@@ -7,6 +7,16 @@ module ApiController
       def copy_base_controller
         template "base_controller.rb", "app/controllers/api/base_controller.rb"
       end
+
+      def add_inflection_rule
+        insert_into_file "config/initializers/inflections.rb", :after => "# These inflection rules are supported but not enabled by default:\n" do
+<<CODE
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym 'API'
+end
+CODE
+        end
+      end
     end
   end
 end
