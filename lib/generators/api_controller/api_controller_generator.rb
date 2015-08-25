346944bae30c16dc840c2806fd7b4160ec832002
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-08-25 07:11:26 +0530

    generate routes

--- lib/generators/api_controller/api_controller_generator.rb
+++ lib/generators/api_controller/api_controller_generator.rb
@@ -10,7 +10,17 @@ class ApiControllerGenerator < Rails::Generators::NamedBase
   class_option :orm, banner: "NAME", type: :string, required: true,
     desc: "ORM to generate the controller for"
 
-  def create_controller_files
+  def create_controller_file
     template "controller.rb", File.join('app/controllers/api', controller_class_path, "#{controller_file_name}_controller.rb")
+  end
+
+  def add_routes_entry
+    insert_into_file "config/routes.rb", :after => "Rails.application.routes.draw do\n" do
+      <<CODE
+  namespace :api do
+    resources :#{controller_file_name}
+  end\n
+CODE
+    end
   end
 end
